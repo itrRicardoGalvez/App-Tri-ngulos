e8070d14aeed44cfe9e26e87ce03b6691dded99c
Ricardo <20230332@ricaldone.edu.sv>
2024-03-21 12:10:22 -0600

    App que calcula el àrea

---
+++ app/src/main/java/ricardo/galvez/areadeuntriangulo/Calcular.kt
@@ -0,0 +1,8 @@
+package ricardo.galvez.areadeuntriangulo
+
+class Calcular {
+    fun Multiplicar (Base: Double, Altura: Double  ): Double{
+        var resultado = Base * Altura / 2
+        return resultado
+    }
+}
---
+++ app/src/main/java/ricardo/galvez/areadeuntriangulo/MainActivity.kt
@@ -0,0 +1,34 @@
+package ricardo.galvez.areadeuntriangulo
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val txtBase = findViewById<TextView>(R.id.txtBase)
+        val txtAltura = findViewById<TextView>(R.id.txtBase)
+        val txtResultado = findViewById<TextView>(R.id.txtResultado)
+        val btnCalcular = findViewById<Button>(R.id.btnCalcular)
+        val obj1 = Calcular()
+
+        btnCalcular.setOnClickListener {
+            val result = obj1.Multiplicar(txtAltura.text.toString().toDouble(), txtBase.text.toString().toDouble())
+            txtResultado.text = "El àrea del triàngulo es $result "
+            Toast.makeText(this, "El àrea del triàngulo es: $result", Toast.LENGTH_LONG).show()
+        }
+    }
+}
